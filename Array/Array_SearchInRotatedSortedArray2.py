e6862fda07f5a3046415543f18bf1b75189617ca
Ravikumar <ravikumar.pandey@cimpress.com>
2024-12-07 15:20:37 +0530

    Added Array_SearchInRotatedSortedArray2

--- Array/Array_SearchInRotatedSortedArray2.py
+++ Array/Array_SearchInRotatedSortedArray2.py
@@ -1,15 +1,11 @@
-# Search In Rotated Sorted Array Part I
+# Search in Rotated Sorted Array II
 
-# Brute Force: Linear Search
+# Brute Force Approach: Linear Search
 # def search(nums, target):
 #     for i in range(len(nums)):
 #         if nums[i] == target:
-#             return i
-#     return -1
-
-# a = [4,5,6,7,0,1,2]
-# print(search(a, 0))
-
+#             return True
+#     return False
 
 # Optimized: Modified Optimized Binary Search
 class Solution:
@@ -19,7 +15,11 @@ class Solution:
         while (low <= high):
             mid = (low + high) // 2
             if (nums[mid] == target):
-                return mid
+                return True
+            if (nums[low] == nums[mid] and nums[mid] == nums[high]):
+                hight = high - 1
+                low = low + 1
+                continue
             if (nums[low] <= nums[mid]):
                 if (nums[low] <= target and target <= nums[mid]):
                     high = mid - 1
@@ -30,7 +30,7 @@ class Solution:
                     low = mid + 1
                 else:
                     high = mid - 1
-        return -1
-
-# Time Complexity: The time complexity of the code is 𝑂(log ⁡𝑛), because it uses binary search to halve the search space in each iteration.
-# Space Complexity: The space complexity of the code is 𝑂(1), because it uses only a constant amount of extra space for variables.
+        return False
+    
+# Time Complexity: The time complexity is 𝑂(𝑛) in the worst case because of the scenario where duplicates make the algorithm reduce both low and high linearly, but 𝑂(log ⁡𝑛) in the average case for binary search in a rotated sorted array.
+# Space Complexity: The space complexity is 𝑂(1), as the algorithm operates in-place with no additional data structures.
